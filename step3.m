ead3fa0dc38e30a4e6cceb2b5da068d11d580026
wakkame <wyuya0410@gmail.com>
2023-04-28 15:06:42 +0900

    可読性向上

--- step3.m
+++ step3.m
@@ -1,12 +1,12 @@
 %ラの音データ読み込み
-%[y,fs] = audioread("A4.wav");
+%[y, fs] = audioread("A4.wav");
 
 %kitamuravoiceの読み込み
-[y,fs] = audioread("kitamuravoice.wav");
-y = (y(:,1) + y(:,2))/2;
+[y, fs] = audioread("kitamuravoice.wav");
+y = ( y(:, 1) + y(:, 2) ) / 2;
 
 %信号長取得
-signalLength = size(y,1);
+signalLength = size(y, 1);
 
 %fftSize定義
 fftSize = 1024;
@@ -24,14 +24,14 @@ numRow = ceil((signalLength - fftSize) / shiftSize) + 1;
 paddingSize = fftSize - 1;
 
 %zeros生成
-padding = zeros(paddingSize,1);
+padding = zeros(paddingSize, 1);
 
 %padding結合
 yPadding = [y;padding];
 
 %spec定義
-spec = zeros(fftSize,numRow);
-
+spec = zeros(fftSize, numRow);
+ 
 for n = 1:numRow
     %yから抽出
     vec = yPadding(1 + (n - 1)*shiftSize:fftSize + (n - 1)*shiftSize,1);
@@ -43,17 +43,17 @@ for n = 1:numRow
     yDft = fft(vecWindow);
 
     %結果格納
-    spec(:,n) = yDft;
+    spec(:, n) = yDft;
 end
 
 %---パワースペクトログラムの表示部---
 
 %絶対値の計算
-specAbs = sqrt(real(spec).^2 + imag(spec).^2);
+ampSpec = sqrt(real(spec).^2 + imag(spec).^2);
 %specAbs = abs(spec);
 
 %db変換
-specDb = 20 * log10(specAbs);
+powerSpec = 20 * log10(ampSpec);
 
 %縦軸生成(周波数[Hz])
 %fsのfftsize分割
@@ -62,12 +62,12 @@ yGrid = linspace(0,fs,fftSize);
 %横軸生成(時間[s])
 %1秒のデータ数=fs個
 %全体の時間=signalLength/fs
-xGrid = linspace(0,signalLength/fs,numRow);
+xGrid = linspace(0, signalLength / fs, numRow);
 
 %スペクトログラム描画
-imagesc(xGrid,yGrid,specDb);
+imagesc(xGrid, yGrid, powerSpec);
 axis xy;
-fontsize(gca,15,"pixels")
+fontsize(gca, 15, "pixels")
 ylabel("周波数 [Hz]", 'FontSize', 18);
 xlabel("時間 [s]", 'FontSize', 18);
 caxis([-60 30]);
